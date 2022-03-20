4c3529b0f20a7c20e17d1c7598da1f80a29a2c47
HitJatin <adriano23012001@gmail.com>
2022-03-21 03:24:36 +0530

    Not allowed from mobile

--- src/components/zone-content/zone-content.js
+++ src/components/zone-content/zone-content.js
@@ -8,6 +8,8 @@ import ZoneSection from "./zone-section/zone-section";
 const ZoneRoute = (props) => {
     const params = useParams();
     const profile = params.profile, roadmap = params.roadmap, pathName = params.pathName, zoneName = params.zoneName;
+    if(window.screen.width<768)
+    return (<div>Coding is supposed to be learned through Laptop/PC. So please switch to Laptop/PC and login again.</div>)
     return (<ZoneContent lessonProgress={props.lessonProgress} updateLessonProgress={props.updateLessonProgress} mainApis={props.mainApis} roomManagementApis={props.roomManagementApis} helpApis={props.helpApis} email={props.email} userName={props.userName} profile={profile} roadmap={roadmap} pathName={pathName} zoneName={zoneName} meetingLink={props.meetingLink} feedbackApis={props.feedbackApis} />)
 }
 
