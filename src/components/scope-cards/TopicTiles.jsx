8f39396a50d56237ee2fdda2a8c00535991b41b1
HitJatin <adriano23012001@gmail.com>
2022-06-17 22:50:18 +0530

    improved ui

--- src/components/scope-cards/TopicTiles.jsx
+++ src/components/scope-cards/TopicTiles.jsx
@@ -4,8 +4,7 @@ import { Card, ProgressBar, Button } from "react-bootstrap";
 class TopicTiles extends Component {
   render() {
     let percentage;
-    if(this.props.progressTopicid)
-    {
+    if(this.props.progressTopicid){
       let count = 0;
       for (let i = 0; i < this.props.progressTopicid.completed.length; i++) {
         if(this.props.progressTopicid.completed[i])
@@ -13,26 +12,21 @@ class TopicTiles extends Component {
       }
       percentage = parseFloat(count/this.props.progressTopicid.completed.length*100).toFixed(2);
     }
-
+    if(percentage == 100)
+    percentage = null;
     return (
       <Card key={this.props.topic.id}
         id={this.props.topic.id}
-        className="cardstyle"
-        style={{
-          width: "10rem",
-          height: "10rem",
-          textAlign: "center",
-          margin: "5px",
-        }}
+        className="topicTiles"
       >
         <Card.Body>
-          <Card.Title className="subjectTitle">
+          <Card.Title className="topicTitle">
             {this.props.topic.name}
           </Card.Title>
-          {percentage ?<div>{ ` ${percentage}% completed`}
-          <ProgressBar variant="success" now={percentage} animated /> </div> : <div></div>}
+          {percentage ?<ProgressBar className="topicProgress" variant="success" now={percentage} animated /> : <div className="topicProgress"></div>}
          
           <Button
+            className="topicButton"
             onClick={() => {
               this.props.changeTopicView(this.props.topic.id, this.props.topic.name);
             }}
